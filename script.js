f3e24d3b861720d23ad80dd51b72d35c012920c8
Adam Sinning <asinning25@gmail.com>
2023-07-29 18:22:45 -0600

    added script.js with parallax effect

---
+++ script.js
@@ -0,0 +1,18 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+    .to('.rock', 3, {y: -300})
+    .to('.girl1', 3, {y: -200}, '-=3')
+    .fromTo('.bg1', {y: -90}, {y: 0, duration: 3}, '-=3')
+    .to('.content', 3, {top: '0%'}, '-=3')
+    .fromTo('.content-images', {opacity: 0}, {opacity: 1, duration: 3});
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration: "400%",
+    triggerHook: 0,
+})
+    .setTween(timeline)
+    .setPin("section")
+    .addTo(controller);
